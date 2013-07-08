5e16eaf3e00920769c5450509d28a4692ec7aba7
Alexey Makhankov <maxho64it@gmail.com>
2013-07-08 07:31:29 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var buf = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+app.get('/', function(request, response){
+    response.send(buf.toString('utf8', 0, len));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
